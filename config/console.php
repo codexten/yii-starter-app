f1464348bef42537a51825ada387eb5ed499a3d4
Jomon Johnson <jomon.entero@gmail.com>
2019-03-06 11:23:31 +0530

    init

---
+++ config/console.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jomon
+ * Date: 26/2/19
+ * Time: 1:42 PM
+ */
+
+return [
+    'controllerMap' => [
+        'migrate' => [
+            'migrationNamespaces' => [
+                'core\migrations',
+            ],
+        ],
+    ],
+];
