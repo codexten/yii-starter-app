a19c051dfd374cfcf08b72338ec9dfc9372701be
Jomon Johnson <jomon.entero@gmail.com>
2019-03-08 21:22:11 +0530

    init

--- config/params.php
+++ config/params.php
@@ -10,4 +10,7 @@ return [
     //bootswatch
     'bootswatch.theme' => 'cosmo',
 
+    'debug.enabled' => $_ENV['DEBUG'],
+    'debug.allowedIps' => $_ENV['DEBUG_ALLOWED_IPS'],
+    'debug.historySize' => 100,
 ];
